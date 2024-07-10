d96657df00b212654f1e5089538a33ae9d2fe5fc
Luis Llanes <luisllaboj@gmail.com>
2024-07-09 18:53:05 -0700

    add wouter router and Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,39 @@
+import { Route } from "wouter";
 import Container from "./components/container";
-import DemoReactQueryMutationOptimisticLS from "./components/demo-users-query-mutation-optimistic-LS/demo-react-query-mutation-optimistic-LS";
 import GlobalFetchingIndicator from "./components/global-fetching-indicator";
 import GlobalMutatingIndicator from "./components/global-mutating-indicator";
+import Navbar from "./components/Navbar";
 import FilterProvider from "./hooks/context/filter-provider";
+import DemoReactQuery from "./components/demo-users-query/demo-react-query";
+import DemoReactQueryMutationOptimisticLS from "./components/demo-users-query-mutation-optimistic-LS/demo-react-query-mutation-optimistic-LS";
+import DemoReactQueryMutationLS from "./components/demo-users-query-mutation-LS/demo-react-query-mutation-LS";
 
-function App() {
+export default function App() {
   return (
-    <Container>
-      <h1 className="mb-12 text-4xl font-bold">React Query v5</h1>
-
-      <GlobalFetchingIndicator />
-      <GlobalMutatingIndicator />
-
-      <FilterProvider>
-        <DemoReactQueryMutationOptimisticLS />
-      </FilterProvider>
-    </Container>
+    <>
+      <Navbar />
+      <Container>
+        <Router />
+        <GlobalFetchingIndicator />
+        <GlobalMutatingIndicator />
+      </Container>
+    </>
   );
 }
 
-export default App;
+function Router() {
+  return (
+    <>
+      <Route path="/">
+        <h1>Tanstack Query v5 demo</h1>
+      </Route>
+      <Route path={"/query"} component={DemoReactQuery} />
+      <Route path={"/mutation"} component={DemoReactQueryMutationLS} />
+      <Route path={"/optimistic-mutation"}>
+        <FilterProvider>
+          <DemoReactQueryMutationOptimisticLS />
+        </FilterProvider>
+      </Route>
+    </>
+  );
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,86 @@
+import { Link, useLocation } from "wouter";
+import { cn } from "../lib/utils";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { PanelLeft } from "lucide-react";
+
+export default function Navbar() {
+  return (
+    <div className="sticky border-b-2 bg-neutral-900 py-4">
+      <div className="container flex items-center justify-between gap-4">
+        <MobileNavbar />
+        <Link
+          href="/"
+          className="mr-auto inline-block bg-gradient-to-r from-red-500 to-orange-400 bg-clip-text text-xl font-bold text-transparent"
+        >
+          TanStack Query{" "}
+          <span className="align-top text-sm text-white">v5</span> demo
+        </Link>
+        <DesktopNavbar />
+      </div>
+    </div>
+  );
+}
+
+function DesktopNavbar() {
+  return (
+    <nav className="hidden justify-between gap-4 md:flex">
+      <NavbarItem href="/query">query</NavbarItem>
+      <NavbarItem href="/mutation">mutation</NavbarItem>
+      <NavbarItem href="/optimistic-mutation">optimistic mutation</NavbarItem>
+    </nav>
+  );
+}
+
+function MobileNavbar() {
+  return (
+    <Sheet>
+      <SheetTrigger asChild className="md:hidden">
+        <PanelLeft className="size-5" />
+      </SheetTrigger>
+
+      <SheetContent side={"left"} className="max-w-xs">
+        <nav className="flex max-w-xs flex-col gap-8">
+          <Link
+            href="/"
+            className="inline-block bg-gradient-to-r from-red-500 to-orange-400 bg-clip-text text-xl font-bold text-transparent"
+          >
+            TanStack Query{" "}
+            <span className="align-top text-sm text-white">v5</span> demo
+          </Link>
+          <div className="flex flex-col gap-4">
+            <NavbarItem href="/query">query</NavbarItem>
+            <NavbarItem href="/mutation">mutation</NavbarItem>
+            <NavbarItem href="/optimistic-mutation">
+              optimistic mutation
+            </NavbarItem>
+          </div>
+        </nav>
+      </SheetContent>
+    </Sheet>
+  );
+}
+
+function NavbarItem({
+  href,
+  children,
+  className,
+}: {
+  href: string;
+  children: React.ReactNode;
+  className?: string;
+}) {
+  const [pathname] = useLocation();
+  return (
+    <Link
+      href={href}
+      className={cn(
+        "font-semibold",
+        pathname === href &&
+          "bg-gradient-to-r from-red-500 to-orange-400 bg-clip-text text-transparent",
+        className,
+      )}
+    >
+      {children}
+    </Link>
+  );
+}
